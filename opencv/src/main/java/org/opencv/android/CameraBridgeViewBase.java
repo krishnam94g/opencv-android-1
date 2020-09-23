fe4c9fc0a17a8965bc760a0cbd63024f99d07da0
krishnam gupta@mfine <krishnam.gupta@mfine.co>
2020-09-23 18:43:00 +0530

    res fix

--- opencv/src/main/java/org/opencv/android/CameraBridgeViewBase.java
+++ opencv/src/main/java/org/opencv/android/CameraBridgeViewBase.java
@@ -561,8 +561,8 @@ public abstract class CameraBridgeViewBase extends SurfaceView implements Surfac
             }
         }
   //      return new Size(1080, 1920);
-          calcWidth = 1080;
-        calcHeight = 1920;
+        //   calcWidth = 1080;
+        // calcHeight = 1920;
 
       return new Size(calcWidth, calcHeight);
     }
